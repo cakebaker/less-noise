1e98f50a35094e7fe3013e23793e92b2d66bd07a
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2011-01-21 10:47:27 +0100

    Refactoring tests

--- test/parser-test.js
+++ test/parser-test.js
@@ -1,39 +1,39 @@
-var Parser = require('../parser').Parser;
+var testCase = require('nodeunit').testCase,
+    Parser = require('../parser').Parser;
 
-exports['ignore carriage return newline'] = function (test) {
-    var parser = new Parser();
-    test.strictEqual(false, parser.parse('\r\n'));
-    test.done();
-};
-
-exports['parse a friends response'] = function (test) {
-    test.expect(1);
-    var parser = new Parser();
-    parser.on('friends', function (friends) {
-        test.deepEqual([1, 2, 3], friends);
-    });
-    parser.parse('{"friends":[1,2,3]}');
-    test.done();
-};
-
-exports['parse a status'] = function (test) {
-    test.expect(1);
-    var parser = new Parser();
-    parser.on('status', function (status) {
-        var expectedObject = { text: "foo" };
-        test.deepEqual(expectedObject, status);
-    });
-    parser.parse('{"text":"foo"}');
-    test.done();
-};
-
-exports['parse a retweet'] = function (test) {
-    test.expect(1);
-    var parser = new Parser();
-    parser.on('retweet', function (retweet) {
-        var expectedObject = { text: "foo", retweeted_status: {} };
-        test.deepEqual(expectedObject, retweet);
-    });
-    parser.parse('{"text":"foo","retweeted_status":{}}');
-    test.done();
-};
+exports['Parser#parse'] = testCase({
+    setUp: function (callback) {
+        this.parser = new Parser();
+        callback();
+    },
+    'ignore carriage return newline': function (test) {
+        test.strictEqual(false, this.parser.parse('\r\n'));
+        test.done();
+    },
+    'recognize a friends response': function (test) {
+        test.expect(1);
+        this.parser.on('friends', function (friends) {
+            test.deepEqual([1, 2, 3], friends);
+        });
+        this.parser.parse('{"friends":[1,2,3]}');
+        test.done();
+    },
+    'recognize a status': function (test) {
+        test.expect(1);
+        this.parser.on('status', function (status) {
+            var expectedObject = { text: "foo" };
+            test.deepEqual(expectedObject, status);
+        });
+        this.parser.parse('{"text":"foo"}');
+        test.done();
+    },
+    'recognize a retweet': function (test) {
+        test.expect(1);
+        this.parser.on('retweet', function (retweet) {
+            var expectedObject = { text: "foo", retweeted_status: {} };
+            test.deepEqual(expectedObject, retweet);
+        });
+        this.parser.parse('{"text":"foo","retweeted_status":{}}');
+        test.done();
+    }
+});
