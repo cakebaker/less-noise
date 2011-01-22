5d6ceb82fc6ebbe3be6a64bf17d9feb3ed577a13
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2011-01-22 16:56:46 +0100

    Initial express skeleton

--- less-noise.js
+++ less-noise.js
@@ -1,5 +1,14 @@
-var config = require('./config').config(),
+var express = require('express'),
+    config = require('./config').config(),
     parser = require('./parser').Parser(),
     twitter = require('./twitter').Twitter(config);
+
+var app = express.createServer();
+
+app.get('/', function (request, response) {
+    response.render('index.jade');
+});
+
+app.listen(config.port);
 
 twitter.stream(parser);
