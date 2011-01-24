9c0907e2655d8d0ea311599cfbf7aedade8dbcbf
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2011-01-24 15:09:20 +0100

    Display avatar and username

--- public/js/less-noise.js
+++ public/js/less-noise.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     var socket = new io.Socket('localhost', {port: 3000});
     socket.connect();
     socket.on('message', function (data) {
-        $('#tweets').prepend('<div class="tweet">' + data.text + '</div>');
+        $('#tweets').prepend('<div class="tweet"><img src="' + data.profile_image_url + '" width="48" height="48" /><strong>' + data.screen_name + '</strong>: ' + data.text + '</div>');
     });
 });
