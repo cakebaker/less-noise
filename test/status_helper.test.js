06066c29e59fef1f43de992c32de03cb11571a9c
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2011-02-03 16:26:45 +0100

    Change order of tests

--- test/status_helper.test.js
+++ test/status_helper.test.js
@@ -1,20 +1,6 @@
 var testCase = require('nodeunit').testCase,
     statusHelper = require('../status_helper');
 
-exports['hasUrls'] = testCase({
-    'status without urls': function (test) {
-        var status = { text: 'text without urls', entities: { urls: [] } };
-        test.strictEqual(false, statusHelper.hasUrls(status));
-        test.done();
-    },
-    'status with url': function (test) {
-        const URL = 'http://example.com';
-        var status = { text: 'text with url: ' + URL, entities: { urls: [ { url: URL } ] } };
-        test.strictEqual(true, statusHelper.hasUrls(status));
-        test.done();
-    }
-});
-
 exports['getUrlsAsArray'] = testCase({
     'status without urls': function (test) {
         var status = { text: 'text without urls', entities: { urls: [] } };
@@ -25,6 +11,20 @@ exports['getUrlsAsArray'] = testCase({
         const URL = 'http://example.com';
         var status = { text: 'text with url: ' + URL, entities: { urls: [ { url: URL } ] } };
         test.deepEqual([URL], statusHelper.getUrlsAsArray(status));
+        test.done();
+    }
+});
+
+exports['hasUrls'] = testCase({
+    'status without urls': function (test) {
+        var status = { text: 'text without urls', entities: { urls: [] } };
+        test.strictEqual(false, statusHelper.hasUrls(status));
+        test.done();
+    },
+    'status with url': function (test) {
+        const URL = 'http://example.com';
+        var status = { text: 'text with url: ' + URL, entities: { urls: [ { url: URL } ] } };
+        test.strictEqual(true, statusHelper.hasUrls(status));
         test.done();
     }
 });
