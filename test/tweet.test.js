eaa4492f152e287798f59f9f3b83b7579fbd9e54
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2011-02-09 09:13:03 +0100

    Adding Tweet#getStatus

--- test/tweet.test.js
+++ test/tweet.test.js
@@ -1,14 +1,30 @@
 var testCase = require('nodeunit').testCase,
     Tweet = require('../lib/tweet').Tweet;
 
+exports['Tweet#getStatus'] = testCase({
+    'return status': function (test) {
+        var data = { text: 'hello world' };
+        var tweet = new Tweet(data);
+        test.strictEqual(data, tweet.getStatus());
+        test.done();
+    },
+    'return retweeted status': function (test) {
+        var status = { text: 'hello world' };
+        var data = { retweeted_status: status };
+        var tweet = new Tweet(data);
+        test.strictEqual(status, tweet.getStatus());
+        test.done();
+    }
+});
+
 exports['Tweet#isRetweet'] = testCase({
-    'returns false for a status': function (test) {
+    'return false for a status': function (test) {
         var data = { text: 'hello world' };
         var tweet = new Tweet(data);
         test.strictEqual(false, tweet.isRetweet());
         test.done();
     },
-    'returns true for a retweet': function (test) {
+    'return true for a retweet': function (test) {
         var data = { retweeted_status: { text: 'hello world' } };
         var tweet = new Tweet(data);
         test.strictEqual(true, tweet.isRetweet());
