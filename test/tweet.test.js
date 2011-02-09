f42afc44355a0488b88e1c54cdcf22d3e75e82d1
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2011-02-09 10:19:57 +0100

    Adding Tweet#hasUrls

--- test/tweet.test.js
+++ test/tweet.test.js
@@ -2,13 +2,13 @@ var testCase = require('nodeunit').testCase,
     Tweet = require('../lib/tweet').Tweet;
 
 exports['Tweet#getStatus'] = testCase({
-    'return status': function (test) {
+    'returns status': function (test) {
         var data = { text: 'hello world' };
         var tweet = new Tweet(data);
         test.strictEqual(data, tweet.getStatus());
         test.done();
     },
-    'return retweeted status': function (test) {
+    'returns retweeted status': function (test) {
         var status = { text: 'hello world' };
         var data = { retweeted_status: status };
         var tweet = new Tweet(data);
@@ -17,14 +17,41 @@ exports['Tweet#getStatus'] = testCase({
     }
 });
 
+exports['Tweet#hasUrls'] = testCase({
+    'returns false for status without urls': function (test) {
+        var data = { text: 'hello world', entities: { urls: [] } };
+        var tweet = new Tweet(data);
+        test.strictEqual(false, tweet.hasUrls());
+        test.done();
+    },
+    'returns false for retweet without urls': function (test) {
+        var data = { retweeted_status: { text: 'hello world', entities: { urls: [] } } };
+        var tweet = new Tweet(data);
+        test.strictEqual(false, tweet.hasUrls());
+        test.done();
+    },
+    'returns true for status with url': function (test) {
+        var data = { text: 'an url: http://example.com', entities: { urls: [ { url: 'http://example.com' } ] } };
+        var tweet = new Tweet(data);
+        test.strictEqual(true, tweet.hasUrls());
+        test.done();
+    },
+    'returns true for retweet with url': function (test) {
+        var data = { retweeted_status: { text: 'an url: http://example.com', entities: { urls: [ { url: 'http://example.com' } ] } } };
+        var tweet = new Tweet(data);
+        test.strictEqual(true, tweet.hasUrls());
+        test.done();
+    }
+});
+
 exports['Tweet#isRetweet'] = testCase({
-    'return false for a status': function (test) {
+    'returns false for a status': function (test) {
         var data = { text: 'hello world' };
         var tweet = new Tweet(data);
         test.strictEqual(false, tweet.isRetweet());
         test.done();
     },
-    'return true for a retweet': function (test) {
+    'returns true for a retweet': function (test) {
         var data = { retweeted_status: { text: 'hello world' } };
         var tweet = new Tweet(data);
         test.strictEqual(true, tweet.isRetweet());
