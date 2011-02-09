94380a8b55003d043d89f5eaa7dcaa67535fa5b6
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2011-02-09 11:02:50 +0100

    Using Tweet functionality in UrlFilter

--- test/url_filter.test.js
+++ test/url_filter.test.js
@@ -1,4 +1,5 @@
 var testCase = require('nodeunit').testCase,
+    Tweet = require('../lib/tweet').Tweet,
     filter = require('../lib/url_filter');
 
 const UNWANTED_DOMAIN = 'example.org';
@@ -10,22 +11,22 @@ exports['UrlFilter#accept'] = testCase({
         callback();
     },
     'accepts tweet without urls': function (test) {
-        var tweet = { text: 'hello world', entities: { urls: [] } };
+        var tweet = new Tweet({ text: 'hello world', entities: { urls: [] } });
         test.strictEqual(true, this.urlFilter.accept(tweet));
         test.done();
     },
     'accepts tweet with url not in filter list': function (test) {
-        var tweet = { text: 'an url: http://example.com', entities: { urls: [ { url: 'http://example.com' } ] } };
+        var tweet = new Tweet({ text: 'an url: http://example.com', entities: { urls: [ { url: 'http://example.com' } ] } });
         test.strictEqual(true, this.urlFilter.accept(tweet));
         test.done();
     },
     'doesn\'t accept tweet with http url in filter list': function (test) {
-        var tweet = { text: 'an url: http://' + UNWANTED_DOMAIN, entities: { urls: [ { url: 'http://' + UNWANTED_DOMAIN } ] } };
+        var tweet = new Tweet({ text: 'an url: http://' + UNWANTED_DOMAIN, entities: { urls: [ { url: 'http://' + UNWANTED_DOMAIN } ] } });
         test.strictEqual(false, this.urlFilter.accept(tweet));
         test.done();
     },
     'doesn\'t accept tweet with https url in filter list': function (test) {
-        var tweet = { text: 'an url: https://' + UNWANTED_DOMAIN, entities: { urls: [ { url: 'https://' + UNWANTED_DOMAIN } ] } };
+        var tweet = new Tweet({ text: 'an url: https://' + UNWANTED_DOMAIN, entities: { urls: [ { url: 'https://' + UNWANTED_DOMAIN } ] } });
         test.strictEqual(false, this.urlFilter.accept(tweet));
         test.done();
     }
